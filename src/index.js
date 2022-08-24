07c001fcc5367b7463f51b22a53fe243412b95bd
Clara <claraswanson671@gmail.com>
2022-08-24 10:14:20 +0200

    Fix merge issue

--- src/index.js
+++ src/index.js
@@ -1,82 +1,3 @@
-<<<<<<< HEAD
-"use strict"
-
-/// SETUP ///
-import './style.css';
-import logo from './logo.jpg';
-const axios = require('axios').default;
-
-let randomNumber = Math.floor(Math.random() * 1154);
-let theChosenOne = '';
-const container = document.getElementById('container');
-const card = document.querySelector('.card');
-const pic = document.getElementById('pic');
-const header = document.getElementById('header');
-const content = document.getElementById('poke-info');
-container.appendChild(card);
-card.appendChild(header);
-card.appendChild(pic);
-card.appendChild(content);
-// const pokeLogo = new Image();
-// pokeLogo.src = logo;
-
-
-
-/// API REQUEST ///
-axios.get('https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0')
-
-    .then((res) => {
-
-        const data = res.data.results;
-        
-        for (let pokemon of data) {
-            if (data.indexOf(pokemon) === randomNumber) {
-
-                theChosenOne += pokemon.name;
-                const name = document.createElement('h3');
-                name.innerText = theChosenOne;
-
-                axios.get('https://pokeapi.co/api/v2/pokemon/' + theChosenOne)
-
-                    .then((res) => {
-
-                        const pokemonInfo = res.data.types;
-                        let pokeID = res.data.id;
-
-                        if (pokemonInfo.length === 1) {
-
-                            let pokeType = pokemonInfo[0];
-                            let pokemonType = pokeType.type.name;
-
-                            content.innerText = 'ID: ' + pokeID + '\n Type: ' + pokemonType;
-                            header.appendChild(name);
-
-                        } else if (pokemonInfo.length === 2) {
-
-                            let type1 = pokemonInfo[0];
-                            let type2 = pokemonInfo[1];
-
-                            let pokemonType1 = type1.type.name;
-                            let pokemonType2 = type2.type.name;
-
-                            content.innerText = 'ID: ' + pokeID + '\n Types: ' + pokemonType1 + ' and ' + pokemonType2;
-                            header.appendChild(name);
-
-                        }
-
-                    })
-
-                    .catch((err) => {
-                        console.log(err);
-                    });
-            }
-        }
-
-    })
-
-    .catch((err) => {
-        console.log(err);
-=======
 "use strict"
 
 /// SETUP ///
@@ -150,7 +71,7 @@ axios.get('https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0')
 
                     .catch((err) => {
                         console.log(err);
-                    });
+                    })
             }
         }
 
@@ -158,5 +79,4 @@ axios.get('https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0')
 
     .catch((err) => {
         console.log(err);
->>>>>>> c83c2cabf37629549edc4fbee8d57c73cd7196e4
-    });
+    })
