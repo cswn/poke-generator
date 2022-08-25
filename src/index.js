0af6c525685934a1b3a86c6d43e9cfa90bd96e91
Clara <claraswanson671@gmail.com>
2022-08-25 16:07:57 +0200

    User func call instead of win reload 4 new pokemon

--- src/index.js
+++ src/index.js
@@ -80,8 +80,8 @@ waterType.src = water;
 const axios = require('axios').default;
 ///
 
-let randomNumber = Math.floor(Math.random() * 1154);
-let theChosenOne = '';
+
+// let theChosenOne = '';
 const container = document.getElementById('container');
 const card = document.querySelector('.card');
 const pic = document.getElementById('pic');
@@ -106,115 +106,129 @@ function Pokemon(name, type, id) {
     this.id = id;
     content.innerText = 'ID: ' + id;
     this.pokemonTypeDisplay = function() {
-        console.log('function called');
-    if (type === 'bug') {
-        card.style.background = '#7dff00';
-        typeIcon.appendChild(bugType);
-    } else if (type === 'dark') {
-        card.style.background = '#071416';
-        card.style.color = 'white';
-        typeIcon.appendChild(darkType);
-    } else if (type === 'dragon') {
-        card.style.background = '#4a4c09';
-        typeIcon.appendChild(dragonType);
-    } else if (type === 'electric') {
-        card.style.background = '#4a4c09';
-        typeIcon.appendChild(electricType);
-    } else if (type === 'fairy') {
-        card.style.background = '#ff0698';
-        typeIcon.appendChild(fairyType);
-    } else if (type === 'fighting') {
-        card.style.background = '#ff9c00';
-        typeIcon.appendChild(fightingType);
-    } else if (type === 'fire') {
-        card.style.background = '#ff3b00';
-        typeIcon.appendChild(fireType);
-    } else if (type === 'flying') {
-        card.style.background = '#8b8b93';
-        typeIcon.appendChild(flyingType);
-    } else if (type === 'ghost') {
-        card.style.background = '#BC00E5';
-        typeIcon.appendChild(ghostType);
-    } else if (type === 'grass') {
-        card.style.background = '#b1ff00';
-        typeIcon.appendChild(grassType);
-    } else if (type === 'ground') {
-        card.style.background = '#e69138';
-        typeIcon.appendChild(groundType);
-    } else if (type === 'ice') {
-        card.style.background = '#7FC8FF';
-        typeIcon.appendChild(iceType);
-    } else if (type === 'normal') {
-        card.style.background = '#d6d6d6';
-        typeIcon.appendChild(normalType);
-    } else if (type === 'poison') {
-        card.style.background = '#aa2fef';
-        typeIcon.appendChild(poisonType);
-    } else if (type === 'psychic') {
-        card.style.background = '#a52ad2';
-        typeIcon.appendChild(psychicType);
-    } else if (type === 'rock') {
-        card.style.background = '#b45f06';
-        typeIcon.appendChild(rockType);
-    } else if (type === 'steel') {
-        card.style.background = '#6b6b6b';
-        typeIcon.appendChild(steelType);
-    } else if (type === 'water') {
-        card.style.background = '#19d3ff';
-        typeIcon.appendChild(waterType);
-    } 
+        console.log('pokemonTypeDisplay function called');
+        if (type === 'bug') {
+            card.style.background = '#7dff00';
+            typeIcon.appendChild(bugType);
+        } else if (type === 'dark') {
+            card.style.background = '#071416';
+            card.style.color = 'white';
+            typeIcon.appendChild(darkType);
+        } else if (type === 'dragon') {
+            card.style.background = '#4a4c09';
+            typeIcon.appendChild(dragonType);
+        } else if (type === 'electric') {
+            card.style.background = '#4a4c09';
+            typeIcon.appendChild(electricType);
+        } else if (type === 'fairy') {
+            card.style.background = '#ff0698';
+            typeIcon.appendChild(fairyType);
+        } else if (type === 'fighting') {
+            card.style.background = '#ff9c00';
+            typeIcon.appendChild(fightingType);
+        } else if (type === 'fire') {
+            card.style.background = '#ff3b00';
+            typeIcon.appendChild(fireType);
+        } else if (type === 'flying') {
+            card.style.background = '#8b8b93';
+            typeIcon.appendChild(flyingType);
+        } else if (type === 'ghost') {
+            card.style.background = '#BC00E5';
+            typeIcon.appendChild(ghostType);
+        } else if (type === 'grass') {
+            card.style.background = '#b1ff00';
+            typeIcon.appendChild(grassType);
+        } else if (type === 'ground') {
+            card.style.background = '#e69138';
+            typeIcon.appendChild(groundType);
+        } else if (type === 'ice') {
+            card.style.background = '#7FC8FF';
+            typeIcon.appendChild(iceType);
+        } else if (type === 'normal') {
+            card.style.background = '#d6d6d6';
+            typeIcon.appendChild(normalType);
+        } else if (type === 'poison') {
+            card.style.background = '#aa2fef';
+            typeIcon.appendChild(poisonType);
+        } else if (type === 'psychic') {
+            card.style.background = '#a52ad2';
+            typeIcon.appendChild(psychicType);
+        } else if (type === 'rock') {
+            card.style.background = '#b45f06';
+            typeIcon.appendChild(rockType);
+        } else if (type === 'steel') {
+            card.style.background = '#6b6b6b';
+            typeIcon.appendChild(steelType);
+        } else if (type === 'water') {
+            card.style.background = '#19d3ff';
+            typeIcon.appendChild(waterType);
+        } 
     }
 }
 
 
 /// REQUEST ///
 
-axios.get('https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0').then((res) => {
+function generateNewPokemon() {
 
-    const data = res.data.results;
+    // initialize variables used in request
+    let theChosenOne = '';
+    let randomNumber = Math.floor(Math.random() * 1154);
+
+    // make http request with axios
+    axios.get('https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0').then((res) => {
+
+        const data = res.data.results;
         
-    for (let pokemon of data) {
-        if (data.indexOf(pokemon) === randomNumber) {
-            theChosenOne += pokemon.name;
-            name.innerText = theChosenOne;
-
-            return axios.get('https://pokeapi.co/api/v2/pokemon/' + theChosenOne)
+        for (let pokemon of data) {
+            
+            if (data.indexOf(pokemon) === randomNumber) {
+                theChosenOne += pokemon.name;
+                name.innerText = theChosenOne;
+                return axios.get('https://pokeapi.co/api/v2/pokemon/' + theChosenOne)
+            }
         }
-    }
+    
+    }).then((res) => {
+                            
+        let pokeID = res.data.id;
+        const pokemonInfo = res.data.types;
+            
+        if (pokemonInfo.length === 1) {
+    
+            let pokeType = pokemonInfo[0];
+            let pokemonType = pokeType.type.name;
+            const currentPokemon = new Pokemon(theChosenOne, pokemonType, pokeID);
+            currentPokemon.pokemonTypeDisplay();
+    
+        } else if (pokemonInfo.length === 2) {
+    
+            let type1 = pokemonInfo[0];
+            let type2 = pokemonInfo[1];
+    
+            let pokemonType1 = type1.type.name;
+            let pokemonType2 = type2.type.name;
+            const currentPokemon = new Pokemon(theChosenOne, pokemonType1, pokeID);
+            currentPokemon.pokemonTypeDisplay();
+    
+        }
+    
+    }).catch((err) => {
+        console.log(err);
+    })
+    
+    .catch((err) => {
+         console.log(err);
+    })
+    
+}
 
-}).then((res) => {
-                        
-    let pokeID = res.data.id;
-    const pokemonInfo = res.data.types;
-        
-    if (pokemonInfo.length === 1) {
-
-        let pokeType = pokemonInfo[0];
-        let pokemonType = pokeType.type.name;
-        const currentPokemon = new Pokemon(theChosenOne, pokemonType, pokeID);
-        currentPokemon.pokemonTypeDisplay();
-
-    } else if (pokemonInfo.length === 2) {
-
-        let type1 = pokemonInfo[0];
-        let type2 = pokemonInfo[1];
-
-        let pokemonType1 = type1.type.name;
-        let pokemonType2 = type2.type.name;
-        const currentPokemon = new Pokemon(theChosenOne, pokemonType1, pokeID);
-        currentPokemon.pokemonTypeDisplay();
-
-    }
-
-}).catch((err) => {
-    console.log(err);
-})
-
-.catch((err) => {
-     console.log(err);
-})
-
+// call function on initial page load
+generateNewPokemon();
 
 reload.addEventListener('click', () => {
-    window.location.reload();
+
+    typeIcon.removeChild(typeIcon.firstElementChild);
+    generateNewPokemon();
+    
 }) 
